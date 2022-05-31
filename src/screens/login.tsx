7cb75c08cf1dc89e02d381c088301c5b4cf03e0b
Wes Johnson <wes@swj.io>
2022-05-30 20:18:18 -0400

    avoid hardcoding login host

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,6 +1,7 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 import {SolidButton} from '../components/SolidButton';
 import {Type} from '../components/Type';
 import {oauthScopes} from '../constants';
@@ -12,21 +13,37 @@ import {centered, flex} from '../utils/styles';
 export const Login = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, 'Login'>) => {
+  const [host, setHost] = useState('');
   const styles = useThemeStyle(styleCreator);
 
   const onLogin = () => {
-    navigation.navigate('Authorize', {host: 'swj.io', scope: oauthScopes});
+    navigation.navigate('Authorize', {host, scope: oauthScopes});
   };
 
   return (
     <View style={[flex, centered]}>
       <Type scale="XL">Welcome!</Type>
       <View style={styles.spacer} />
-      <SolidButton onPress={onLogin}>Login</SolidButton>
+      <TextInput
+        value={host}
+        onChangeText={setHost}
+        style={styles.input}
+        placeholder="example.com"
+      />
+      <View style={styles.spacer} />
+      <SolidButton onPress={onLogin} disabled={!host}>
+        Login
+      </SolidButton>
     </View>
   );
 };
 
-const styleCreator: StyleCreator = () => ({
+const styleCreator: StyleCreator = ({getColor}) => ({
   spacer: {height: 20},
+  input: {
+    backgroundColor: getColor('baseAccent'),
+    width: '80%',
+    borderRadius: 10,
+    paddingHorizontal: 16,
+  },
 });
