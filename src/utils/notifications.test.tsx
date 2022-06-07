67d781023038cd6452f72569c80a9da1d9c14cff
Wes Johnson <wes@swj.io>
2022-06-07 07:55:33 -0400

    chore(notifs): rm hooks test in favour of render specs

--- src/utils/notifications.test.tsx
+++ src/utils/notifications.test.tsx
@@ -1,234 +1,99 @@
 import React from 'react';
-import MockDate from 'mockdate';
-import {NavigationContainer} from '@react-navigation/native';
-import {act, renderHook} from '@testing-library/react-hooks';
+import {Text, View} from 'react-native';
+import {render, waitFor} from '@testing-library/react-native';
 import {NotificationProvider, useNotifications} from './notifications';
-import * as ApiHooks from '../api/hooks';
-import * as NotificationStorage from '../storage/notifications';
+import {MastodonApiClient} from '../api/mastodon';
+import {MastodonProvider} from '../api/mastodon-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {ApiResponse} from '../api/response';
 
 jest.mock('react-native-mmkv');
-jest.mock('../storage/notifications');
 
-describe('useNotifications', () => {
-  let fetchSpy: jest.SpyInstance;
-  let initialDateTime: number;
+describe('notifications e2e', () => {
+  const TestComponent = () => {
+    const {tabRead, newNotifCount, loadingNotifications} = useNotifications();
+    return (
+      <View>
+        {loadingNotifications ? <Text>Loading</Text> : <Text>Loaded</Text>}
+        <Text>tabRead: {tabRead.toString()}</Text>
+        <Text>newNotifCount: {newNotifCount}</Text>
+      </View>
+    );
+  };
+
+  const mockApi = new MastodonApiClient();
+
+  let getSpy: jest.SpyInstance;
+
+  const Providers = ({children}: {children: JSX.Element}) => (
+    <MastodonProvider value={mockApi}>
+      <NavigationContainer>
+        <NotificationProvider>{children}</NotificationProvider>
+      </NavigationContainer>
+    </MastodonProvider>
+  );
 
   beforeEach(() => {
     jest.clearAllMocks();
-
-    initialDateTime = Date.now();
-    MockDate.set(initialDateTime);
-
-    fetchSpy = jest.fn(() => Promise.resolve());
-    // @ts-expect-error testing subset of api client
-    jest.spyOn(ApiHooks, 'useMyMastodonInstance').mockImplementation(() => ({
-      getNotifications: fetchSpy,
-    }));
+    getSpy = jest
+      .spyOn(mockApi, 'authedGet')
+      .mockResolvedValue({ok: true, body: []} as ApiResponse);
   });
 
-  const wrapper = ({children}: any) => (
-    <NotificationProvider>
-      <NavigationContainer>{children}</NavigationContainer>
-    </NotificationProvider>
-  );
-
-  describe('with no notifications pending', () => {
-    it('should indicate no notifications', () => {
-      const {result} = renderHook(() => useNotifications(), {wrapper});
-
-      expect(result.current).toEqual(
-        expect.objectContaining({
-          newNotifCount: 0,
-          notifs: {},
-          tabRead: true,
-        }),
-      );
-    });
-
-    it('should fetch notifications', () => {
-      renderHook(() => useNotifications(), {wrapper});
-
-      expect(fetchSpy).toHaveBeenCalled();
+  describe('base case', () => {
+    it('should indicate no notifications', async () => {
+      const result = render(<TestComponent />, {wrapper: Providers});
+      await waitFor(() => result.getByText('Loaded'));
+      expect(result.toJSON()).toMatchInlineSnapshot(`
+        <View>
+          <Text>
+            Loaded
+          </Text>
+          <Text>
+            tabRead: 
+            true
+          </Text>
+          <Text>
+            newNotifCount: 
+            0
+          </Text>
+        </View>
+      `);
     });
   });
 
-  describe('having recently fetched notifications', () => {
+  describe('with notifications returned', () => {
     beforeEach(() => {
-      // @ts-expect-error mock
-      NotificationStorage.getLastFetch.mockReturnValue(initialDateTime);
-
-      MockDate.set(initialDateTime + 10000);
+      getSpy.mockResolvedValue({
+        ok: true,
+        body: [
+          {
+            id: '221',
+            created_at: '2022-06-05T17:08:39.145Z',
+            type: 'favourite',
+          },
+        ],
+      } as ApiResponse);
     });
 
-    it('should not call fetch', () => {
-      renderHook(() => useNotifications(), {wrapper});
-
-      expect(fetchSpy).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('having not recently fetched notifications', () => {
-    beforeEach(() => {
-      // @ts-expect-error mock
-      NotificationStorage.getLastFetch.mockReturnValue(initialDateTime);
-
-      MockDate.set(initialDateTime + 200000);
-    });
-
-    it('should call fetch', () => {
-      renderHook(() => useNotifications(), {wrapper});
-
-      expect(fetchSpy).toHaveBeenCalled();
-    });
-  });
-
-  describe('user tab read state', () => {
-    let fetchPromise: Promise<any>;
-
-    beforeEach(() => {
-      // @ts-expect-error mock
-      NotificationStorage.getLastFetch.mockReturnValue(initialDateTime);
-
-      MockDate.set(initialDateTime + 200000);
-    });
-
-    describe('has no prior watermarks', () => {
-      beforeEach(() => {
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({});
-
-        fetchPromise = new Promise(resolve => resolve([]));
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should return tabRead=true', () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-
-        expect(result.current.tabRead).toBe(true);
-      });
-    });
-
-    describe('has the same watermarks', () => {
-      beforeEach(() => {
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({follow: '101'});
-
-        fetchPromise = new Promise(resolve =>
-          resolve([{type: 'follow', id: '101'}]),
-        );
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should return tabRead=true', async () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-        await act(() => fetchPromise);
-
-        expect(result.current.tabRead).toBe(true);
-      });
-    });
-
-    describe('has new values in the incoming watermark', () => {
-      beforeEach(() => {
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({follow: '101'});
-
-        fetchPromise = new Promise(resolve =>
-          resolve([
-            {type: 'follow', id: '101', key: 1653566331400}, // assert sorting reverses this
-            {type: 'follow', id: '102', key: 1653566331488},
-          ]),
-        );
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should return tabRead=false', async () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-        await act(() => fetchPromise);
-
-        expect(result.current.tabRead).toBe(false);
-      });
-    });
-
-    describe('has no value for type in the incoming watermark', () => {
-      beforeEach(() => {
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({follow: '101'});
-
-        fetchPromise = new Promise(resolve =>
-          resolve([
-            {type: 'somethingelse', id: '102'},
-            {type: 'somethingelse', id: '101'},
-          ]),
-        );
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should return tabRead=true', async () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-        await act(() => fetchPromise);
-
-        expect(result.current.tabRead).toBe(true);
-      });
-    });
-
-    describe('has new values in the incoming watermark and none in the saved watermark', () => {
-      beforeEach(() => {
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({});
-
-        fetchPromise = new Promise(resolve =>
-          resolve([
-            {type: 'follow', id: '100'},
-            {type: 'follow', id: '99'},
-            {type: 'follow', id: '98'},
-          ]),
-        );
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should return tabRead=false', async () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-        await act(() => fetchPromise);
-
-        expect(result.current.newNotifCount).toBe(3);
-        expect(result.current.tabRead).toBe(false);
-      });
-    });
-  });
-
-  describe('read state on notif types', () => {
-    let fetchPromise: Promise<any>;
-
-    beforeEach(() => {
-      // @ts-expect-error mock
-      NotificationStorage.getLastFetch.mockReturnValue(initialDateTime);
-
-      MockDate.set(initialDateTime + 200000);
-    });
-
-    describe('receiving the same entries for a previously read type', () => {
-      beforeEach(() => {
-        const follows = [
-          {type: 'follow', id: '102'},
-          {type: 'follow', id: '101'},
-        ];
-
-        // @ts-expect-error mock
-        NotificationStorage.getNotifWatermarks.mockReturnValue({follow: '102'});
-        // @ts-expect-error mock
-        NotificationStorage.getNotifGroup.mockReturnValue({follow: follows});
-
-        fetchPromise = new Promise(resolve => resolve(follows));
-        fetchSpy.mockReturnValue(fetchPromise);
-      });
-
-      it('should not indicate there are more unread', async () => {
-        const {result} = renderHook(() => useNotifications(), {wrapper});
-        await act(() => fetchPromise);
-
-        expect(result.current.tabRead).toBe(true);
-        expect(result.current.notifs.follow).toHaveLength(0);
-      });
+    it('should indicate one new notification', async () => {
+      const result = render(<TestComponent />, {wrapper: Providers});
+      await waitFor(() => result.getByText('Loaded'));
+      expect(result.toJSON()).toMatchInlineSnapshot(`
+        <View>
+          <Text>
+            Loaded
+          </Text>
+          <Text>
+            tabRead: 
+            true
+          </Text>
+          <Text>
+            newNotifCount: 
+            1
+          </Text>
+        </View>
+      `);
     });
   });
 });
